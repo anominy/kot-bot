bde9ee54c160e04fc88802afe27ee77bc9f54e07
anominy <140955338+anominy@users.noreply.github.com>
2024-10-08 16:38:32 +0300

    Use image set from anominy/kitty-images

--- src/main/java/io/github/anominy/kotbot/interaction/command/CatDiscordSlashCommand.java
+++ src/main/java/io/github/anominy/kotbot/interaction/command/CatDiscordSlashCommand.java
@@ -16,81 +16,46 @@
 
 package io.github.anominy.kotbot.interaction.command;
 
-import com.grack.nanojson.JsonArray;
-import com.grack.nanojson.JsonObject;
-import com.grack.nanojson.JsonParser;
-import com.grack.nanojson.JsonParserException;
 import io.github.anominy.jda.prelude.interaction.command.IDiscordSlashCommand;
-import io.github.anominy.jda.prelude.interaction.command.IDiscordSlashCommandOption;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.dv8tion.jda.api.utils.FileUpload;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public final class CatDiscordSlashCommand implements IDiscordSlashCommand {
+    private static final int IMAGE_MIN_INDEX = 1;
+    private static final int IMAGE_MAX_INDEX = 500;
+
     @Override
     public SlashCommandData initDiscordSlashCommandData() {
         return Commands.slash("cat", "Cat!");
     }
 
     @Override
-    public IDiscordSlashCommandOption[] getDiscordSlashCommandOptionArray() {
-        return new IDiscordSlashCommandOption[] {
-                new CountDiscordSlashCommandOption()
-        };
-    }
-
-    @Override
     public void onDiscordSlashCommandInteractionEvent(SlashCommandInteractionEvent event) {
         event.deferReply()
                 .queue(it -> {
-                    Integer count = event.getOption(CountDiscordSlashCommandOption.NAME,
-                            1, OptionMapping::getAsInt);
+                    int index = (int) ((Math.random() * (IMAGE_MAX_INDEX - IMAGE_MIN_INDEX)) + IMAGE_MIN_INDEX);
 
-                    JsonArray jsonArray;
-                    try {
-                        jsonArray = JsonParser.array()
-                                .from(new URL("https://api.thecatapi.com/v1/images/search?limit=" + count));
-                    } catch (JsonParserException | MalformedURLException ignored) {
-                        return;
-                    }
+                    String url = "https://raw.githubusercontent.com/anominy/kitty-images/refs/heads/main/"
+                            + index
+                            + ".jpg";
 
-                    FileUpload[] fileUploads = new FileUpload[count];
-                    for (int i = 0; i < fileUploads.length; ++i) {
-                        JsonObject jsonObject = jsonArray.getObject(i);
-                        String url = jsonObject.getString("url");
-                        try (InputStream is = new URL(url)
-                                .openStream()) {
-                            String[] split = url.split("/");
-                            String name = split[split.length - 1];
+                    try (InputStream is = new URL(url)
+                            .openStream()) {
+                        String[] split = url.split("/");
+                        String name = split[split.length - 1];
 
-                            //noinspection resource
-                            fileUploads[i] = FileUpload.fromData(is.readAllBytes(), name);
-                        } catch (IOException ignored) {
+                        try (FileUpload fileUpload = FileUpload.fromData(is.readAllBytes(), name)) {
+                            it.sendFiles(fileUpload)
+                                    .queue();
                         }
+                    } catch (IOException ignored) {
                     }
-
-                    it.sendFiles(fileUploads)
-                            .queue();
                 });
-    }
-
-    private static final class CountDiscordSlashCommandOption implements IDiscordSlashCommandOption {
-        public static final String NAME = "count";
-
-        @Override
-        public OptionData initDiscordSlashCommandOptionData() {
-            return new OptionData(OptionType.INTEGER, NAME, "Count!")
-                    .setMinValue(1)
-                    .setMaxValue(10);
-        }
     }
 }
